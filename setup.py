f28d5e83340cef6ef5420efe49c29953192c7a1d
Sebastian Hoefler <sebh@Sebastians-Mini.fritz.box>
2023-11-08 19:08:55 +0100

    fixed a typo in email

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     url='https://github.com/SebastianHoefler/dsss_homework_2.git',
     author='Sebastian Hoefer',
-    author_email='sebstian.u.hoefler@gmail.com',
+    author_email='sebastian.u.hoefler@gmail.com',
     description='Math Quiz',
     packages=find_packages(),    
     install_requires=['numpy >= 1.11.1'],
