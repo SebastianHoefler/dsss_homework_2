8942ce5c1f518e730efc6ad320caef937ce9a031
Sebastian Hoefler <sebh@Sebastians-Mini.fritz.box>
2023-11-08 19:06:08 +0100

    changed innit file

--- math_quiz/__innit__.py
+++ math_quiz/__innit__.py
@@ -1,3 +1,8 @@
-from math_quiz.math_quiz import *
-from math_quiz.tests_math_quiz import * 
+# Contents of your_package/__init__.py
 
+print("your_package has been imported")
+
+__all__ = ["math_quiz", "tests_math_quiz"]
+
+from . import math_quiz
+from . import tests_math_quiz
