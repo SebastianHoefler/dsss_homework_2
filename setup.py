9017758a64f03432cb6a514f82b1a24f1594f2ae
Sebastian Hoefler <sebh@Sebastians-Mini.fritz.box>
2023-11-09 10:19:09 +0100

    changed setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dsss_homework_2',
+    name='math_quiz',
     version='1.0.0',
     url='https://github.com/SebastianHoefler/dsss_homework_2.git',
-    author='Sebastian Hoefer',
+    author='A scored mathquiz for elementary math problems',
     author_email='sebastian.u.hoefler@gmail.com',
     description='Math Quiz',
     packages=find_packages(),    
